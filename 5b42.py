f3e3a2cda2bdbc4e3439c88742401812a8775b61
ravuriramyasri <42571523+ravuriramyasri@users.noreply.github.com>
2018-12-06 12:13:39 +0530

    Create 5b42.py

---
+++ 5b42.py
@@ -0,0 +1,9 @@
+string1=map(int,raw_input.split())
+string2=map(int,raw_input.split())
+if(string1 == string2):
+    print string1
+else:
+    if(string1>string2):
+        print string1
+    else:
+        print string2
